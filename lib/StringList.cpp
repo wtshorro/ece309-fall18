b39587c6bc3473077ffa270e822f790d947bf3b4
wtshorro <42945094+wtshorro@users.noreply.github.com>
2018-09-04 14:28:02 -0400

    Add files via upload

--- lib/StringList.cpp
+++ lib/StringList.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 class List {
 private:
@@ -111,12 +111,21 @@ public:
 void printN(int , List*);
 int main(void){
   List* a = new List();
-  a->push_back("string 1");
-  char* data = a->remove_front();
+  char* data = (char*)malloc(sizeof(char)*9);
+  data = (char*)"string 1";
+  a->push_back(data);
+  data = a->remove_front();
+  free(data);
   printf("this is the data removed: %s\n", data);
-  a->push_back("string 2");
-  a->push_back("string 3");
-  a->push_back("string 4");
+  data = (char*)malloc(sizeof(char)*9);
+  data = (char*)"string 2";
+  a->push_back(data);
+  data = (char*)malloc(sizeof(char)*9);
+  data = (char*)"string 3";
+  a->push_back(data);
+  data = (char*)malloc(sizeof(char)*9);
+  data = (char*)"string 4";
+  a->push_back(data);
   printN(0,a);
   printN(1,a);
   printN(2,a);
@@ -136,7 +145,7 @@ void printN(int n, List *a){
       string = a->get(n);
   }
   else{
-      string = "there is no item at this index";
+      string = (char*)"there is no item at this index";
   }
   printf("this is the %d item in the list: %s\n",(n+1), string);
 }
