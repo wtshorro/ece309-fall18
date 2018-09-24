5f6e6ae1df04bd7d8bc99b2b5dbb88366424004c
wtshorro <42945094+wtshorro@users.noreply.github.com>
2018-09-24 17:32:57 -0400

    Added file PListTest.cpp

---
+++ tests/PListTest.cpp
@@ -0,0 +1,29 @@
+//************************** Test Code ***************************//
+#include<stdio.h>
+#include "PList.h"
+
+int main(void){
+  PList a;
+  a.push_back(new StringObject((char*)"I'm a String"));
+  a.push_back(new DoubleObject(14.52));
+  a.push_back(new IntegerObject(22));
+
+  PList::Iterator it = a.begin();
+  it.print();
+  it.decrement();
+  it.print();
+  it.decrement();
+  it.print();
+  it.decrement();
+  it.print();
+  it.increment();
+  it.print();
+  it.increment();
+  it.print();
+  it.increment();
+  it.increment();
+  it.print();
+
+  return 0;
+}
+//************************** End Test Code ***************************//
